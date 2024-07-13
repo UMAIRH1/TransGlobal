2dc48d681f2d57b140fffb22f54476bd9c30af8c
Umair Habib <128183479+UMAIRH1@users.noreply.github.com>
2024-07-13 13:19:45 +0500

    Add files via upload

---
+++ hms/admin/include/sidebar.php
@@ -0,0 +1,124 @@
+<div class="sidebar app-aside" id="sidebar">
+				<div class="sidebar-container perfect-scrollbar">
+
+<nav>
+						<div class="navbar-title">
+							<span>Main Navigation</span>
+						</div>
+						<ul class="main-navigation-menu">
+							<li>
+								<a href="dashboard.php">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-home"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> Dashboard </span>
+										</div>
+									</div>
+								</a>
+							</li>
+							<li>
+								<a href="javascript:void(0)">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-user"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> Drivers </span><i class="icon-arrow"></i>
+										</div>
+									</div>
+								</a>
+								<ul class="sub-menu">
+									<li>
+										<a href="doctor-specilization.php">
+											<span class="title"> Driver Location </span>
+										</a>
+									</li>
+									<li>
+										<a href="add-doctor.php">
+											<span class="title"> Add Driver</span>
+										</a>
+									</li>
+									<li>
+										<a href="Manage-doctors.php">
+											<span class="title"> Manage Drivers </span>
+										</a>
+									</li>
+									
+								</ul>
+								</li>
+
+				<li>
+								<a href="javascript:void(0)">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-user"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> Users </span><i class="icon-arrow"></i>
+										</div>
+									</div>
+								</a>
+								<ul class="sub-menu">
+									
+									<li>
+										<a href="manage-users.php">
+											<span class="title"> Manage Users </span>
+										</a>
+									</li>
+									
+								</ul>
+								</li>
+								<li>
+								<a href="javascript:void(0)">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-user"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> Customers </span><i class="icon-arrow"></i>
+										</div>
+									</div>
+								</a>
+								<ul class="sub-menu">
+									
+								<!-- 	<li>
+										<a href="manage-patient.php">
+											<span class="title"> Manage Patients </span>
+										</a>
+									</li> -->
+									<li>
+										<a href="add-patient.php">
+											<span class="title"> Add Customers</span>
+										</a>
+									</li>
+									<li>
+										<a href="manage-patient.php">
+											<span class="title"> Manage Customer </span>
+										</a>
+									</li>
+									
+								</ul>
+								</li>	
+
+<li>
+								<a href="appointment-history.php">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-file"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> Shipment History </span>
+										</div>
+									</div>
+								</a>
+							</li>
+
+
+	</ul>
+	
+						
+					</nav>
+					</div>
+			</div>
